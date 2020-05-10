3c95604e96094b79ff1958ba13eabbdc68c6f120
CasperLindencrone <64884327+CasperLindencrone@users.noreply.github.com>
2020-05-10 12:27:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let xjson = {
+    "trailer": [
+        {
+            "tra": "Tenacious D in the Pick of Destiny",
+            "link": "TXxQFMG86HA"
+        },
+        {
+            "tra": "Space Jam",
+            "link": "oKNy-MWjkcU"
+        },
+        {
+            "tra": "Ivanhoe",
+            "link": "A8_N5iB2slE"
+        },
+        {
+            "tra": "Saw",
+            "link": "S-1QgOMQ-ls"
+        },
+        {
+            "tra": "The Matrix",
+            "link": "m8e-FF8MsqU"
+        },
+        {
+            "tra": "The Rocky Horror Picture Show",
+            "link": "LnYBwampG8U"
+        },
+        {
+            "tra": "My Name Is Nobody",
+            "link": "2RYq1PLdT0s"
+        },
+        {
+            "tra": "The Shawshank Redemption",
+            "link": "6hB3S9bIaco"
+        },
+        {
+            "tra": "Kung Pow: Enter the Fist",
+            "link": "GXrAYdSeWY8"
+        },
+        {
+            "tra": "American Psycho",
+            "link": "5YnGhW4UEhc"
+        }
+    ]
+}
